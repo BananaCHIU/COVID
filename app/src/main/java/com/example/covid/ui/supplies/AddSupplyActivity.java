5cac16bbfb67569e192178d7c5aefafd34e89f9e
BananaCHIU <46750088+BananaCHIU@users.noreply.github.com>
2020-03-24 00:49:48 +0800

    Added add supply layout only

---
+++ app/src/main/java/com/example/covid/ui/supplies/AddSupplyActivity.java
@@ -0,0 +1,16 @@
+package com.example.covid.ui.supplies;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.covid.R;
+
+public class AddSupplyActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_add_supply);
+    }
+}
